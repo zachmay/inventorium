5924ca559a79cbe2e4b960112bc4eae97c979592
Zachary May <zach@sector42.net>
2015-09-08 20:10:36 -0400

    Updates to routes, models

--- site/Handler/Api.hs
+++ site/Handler/Api.hs
@@ -31,15 +31,56 @@ getReportReconciliationR           = defaultApiHandler
 
 getReportByTypeR                   = defaultApiHandler
 
-getBuildingListR                     = defaultApiHandler
-postBuildingListR                    = defaultApiHandler
-getBuildingR buildingId              = basicApiHandler $ object [ "building" .= buildingId ]
-putBuildingR buildingId              = basicApiHandler $ object [ "building" .= buildingId ]
-deleteBuildingR buildingId           = basicApiHandler $ object [ "building" .= buildingId ]
-getRoomListR buildingId              = basicApiHandler $ object [ "building" .= buildingId ]
-postRoomListR buildingId             = basicApiHandler $ object [ "building" .= buildingId ]
-getRoomR buildingId roomId           = basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
-putRoomR buildingId roomId           = basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
-deleteRoomR buildingId roomId        = basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
-getRoomInventoryR buildingId roomId  = basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
-postRoomInventoryR buildingId roomId = basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
+getBuildingListR :: Handler TypedContent
+getBuildingListR = do
+    buildingEntities <- runDB $ selectList [] [Asc BuildingName]
+    basicApiHandler $ toJSON buildingEntities
+
+{- TODO: Handle duplicate name -}
+{- TODO: Content negotiation, on both request and response -}
+postBuildingListR :: Handler TypedContent
+postBuildingListR = do
+    building <- requireJsonBody :: Handler Building
+    buildingId <- runDB $ insert building
+    sendResponseStatus created201 . toJSON $ Entity buildingId building
+
+getBuildingR :: BuildingId -> Handler TypedContent
+getBuildingR buildingId = do
+    building <- runDB $ get404 buildingId
+    basicApiHandler . toJSON $ Entity buildingId building
+
+putBuildingR :: BuildingId -> Handler TypedContent
+putBuildingR buildingId = 
+    basicApiHandler $ object [ "building" .= buildingId ]
+
+deleteBuildingR :: BuildingId -> Handler TypedContent
+deleteBuildingR buildingId =
+    basicApiHandler $ object [ "building" .= buildingId ]
+
+getRoomListR :: BuildingId -> Handler TypedContent
+getRoomListR buildingId =
+    basicApiHandler $ object [ "building" .= buildingId ]
+
+postRoomListR :: BuildingId -> Handler TypedContent
+postRoomListR buildingId =
+    basicApiHandler $ object [ "building" .= buildingId ]
+
+getRoomR :: BuildingId -> RoomId -> Handler TypedContent
+getRoomR buildingId roomId =
+    basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
+
+putRoomR :: BuildingId -> RoomId -> Handler TypedContent
+putRoomR buildingId roomId =
+    basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
+
+deleteRoomR :: BuildingId -> RoomId -> Handler TypedContent
+deleteRoomR buildingId roomId =
+    basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
+
+getRoomInventoryR :: BuildingId -> RoomId -> Handler TypedContent
+getRoomInventoryR buildingId roomId =
+    basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
+
+postRoomInventoryR :: BuildingId -> RoomId -> Handler TypedContent
+postRoomInventoryR buildingId roomId =
+    basicApiHandler $ object [ "building" .= buildingId, "room" .= roomId ]
